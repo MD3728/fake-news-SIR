fce3c5cf0fe16d58cadf23bd6cab445331f8a569
Moses Daniel Dong <96071319+MD3728@users.noreply.github.com>
2024-03-28 11:35:49 -0400

    Visuals

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Euler Algorithm for a Nonlinear System of DE (Fake News)
+import matplotlib.pyplot as plt
+import numpy as np
 
 def eulerSum(stepInitial, stepFinal, stepSize, cureConstant, infectionConstant, 
              initialSusceptible, initialInfected, initialResistant):
@@ -8,15 +10,36 @@ def eulerSum(stepInitial, stepFinal, stepSize, cureConstant, infectionConstant,
   numResistant = initialResistant
   numTotal = numSusceptible + numInfected + numResistant
   fWrite = open("estimation.txt", "w")
+  susceptibles = []
+  infecteds = []
+  resistants = []
+  steps = []
 
   # Num Interations
   while (stepInitial <= stepFinal):
-    stepValues = equationSystem(cureConstant,infectionConstant, numSusceptible, numInfected, numResistant, numTotal)
+    stepValues = equationSystem(cureConstant, infectionConstant, numSusceptible, numInfected, numResistant, numTotal)
     numSusceptible += stepSize * stepValues[0]
     numInfected += stepSize * stepValues[1]
     numResistant += stepSize * stepValues[2]
     stepInitial += stepSize
-    fWrite.write(f"Step Value {round(stepInitial,3)}: {round(numSusceptible,3)} {round(numInfected,3)} {round(numResistant,3)}\n")
+    roundInit = round(stepInitial,3)
+    roundSusceptible = round(numSusceptible,3)
+    roundInfected = round(numInfected,3)
+    roundResistant = round(numResistant,3)
+    susceptibles.append(roundSusceptible)
+    infecteds.append(roundInfected)
+    resistants.append(roundResistant)
+    steps.append(roundInit)
+
+    fWrite.write(f"Step Value {roundInit}: {roundSusceptible} {roundInfected} {roundResistant}\n")
+
+  plt.plot(steps, susceptibles, label='Susceptible')
+  plt.plot(steps, infecteds, label='Infected')
+  plt.plot(steps, resistants, label='Resistant')
+  plt.xlabel('Time')
+  plt.ylabel('Number of Individuals')
+  plt.legend()
+  plt.show()
 
   fWrite.close()
   
@@ -28,7 +51,7 @@ def equationSystem(cureConstant, infectionConstant, numSusceptible, numInfected,
 
 def main():
   # Testing Parameters
-  eulerSum(0, 30, 0.05, 0.7, 1.34,
+  eulerSum(0, 30, 0.001, 0.07, 1.34,
            134000000,59,13)# Initial Values
 
 if __name__ == "__main__":
